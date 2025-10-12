655dc2e69f7baa25f763143e2372bf54c375ac52
Danil <yalexxer228666@mail.ru>
2025-10-12 22:31:32 +0400

    feat | update new user route

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,13 +1,14 @@
 "use server";
+
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
 import { Prisma } from "@prisma/client";
 
-
 interface PrismaP2002Meta {
   target?: string[];
 }
+
 // Функция для валидации email
 function isValidEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -22,25 +23,20 @@ export async function POST(req: Request) {
   try {
     const { email, login, password, name, avatar } = await req.json();
 
-    // Проверка обязательных полей
     if (!email || !login || !password) {
       return NextResponse.json({ error: "Все поля обязательны" }, { status: 400 });
     }
 
-    // Валидация email
     if (!isValidEmail(email)) {
       return NextResponse.json({ error: "Некорректный email" }, { status: 400 });
     }
 
-    // Валидация пароля
     if (!isValidPassword(password)) {
       return NextResponse.json({ error: "Пароль должен быть не менее 6 символов" }, { status: 400 });
     }
 
-    // Хэшируем пароль
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Создание пользователя с обработкой ошибок уникальности
     try {
       const user = await prisma.user.create({
         data: {
@@ -53,14 +49,13 @@ export async function POST(req: Request) {
       });
 
       return NextResponse.json({ message: "Пользователь успешно создан!", userId: user.id }, { status: 201 });
-    } catch (err) {
-      // Обработка ошибок уникальности (email или login уже заняты)
+    } catch (err: unknown) {
+      // Обработка ошибок уникальности
       if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === "P2002") {
         const meta = err.meta as PrismaP2002Meta;
         const targetField = meta.target?.[0];
         return NextResponse.json({ error: `${targetField} уже занят` }, { status: 400 });
       }
-
       throw err;
     }
   } catch (e) {
