523ec9f194b77977089dd4e164821d7d4eb72a62
Danil <yalexxer228666@mail.ru>
2026-02-20 13:04:51 +0400

    fix(prisma): fix Prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,4 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: "file:./prisma/dev.db", // or libsql:// for Turso
-  },
-});  
+});
