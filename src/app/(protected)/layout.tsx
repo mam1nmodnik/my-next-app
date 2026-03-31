740f4ebcd9ac556bae2a7ddce965b4057b722356
Danil <yalexxer228666@mail.ru>
2026-03-31 17:49:57 +0400

    fix: error deploy in vercel

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,8 @@
+'use client';
 import AppProvider from "@/_providers/app-provider";
 import LayoutWrapper from "@/layout/layout/LayoutWrapper";
 
-export default async function Layout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
